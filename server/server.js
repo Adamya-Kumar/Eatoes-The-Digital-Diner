fa959ba57aa11d4ad68b06e0c048b30af8f13a66
Adamya Kumar <adamyakumar2307@gmail.com>
2025-04-28 19:19:00 +0530

    port change

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORTER || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
